c6bf5fd839489695aa3d958b0ef37432b6a606c2
HidanTF2 <hidan-spy@hotmail.fr>
2016-04-15 02:28:29 +0200

    maj DistributeurController, update

--- app/Http/Controllers/DistributeurController.php
+++ app/Http/Controllers/DistributeurController.php
@@ -43,6 +43,11 @@ class DistributeurController extends Controller
     }
 
     /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     *
      * @SWG\Post(
      *     path="/distributeur",
      *     summary="Ajouter un distributeur.",
@@ -123,7 +128,7 @@ class DistributeurController extends Controller
             'telephone'=>'string',
             'cpostal'=>'string',
             'ville'=>'string',
-            'pays'=>'string',
+            'pays'=>'string'
         ]);
 
         if($validator->fails()){
@@ -134,10 +139,10 @@ class DistributeurController extends Controller
                 ], 422); //HTTP Status code
         }
 
-        $reduc = new Distributeur(Input::all());
+        $distributeur = new Distributeur(Input::all());
 
-        if($reduc->save()){
-            return response()->json($reduc, 200); //HTTP Status code
+        if($distributeur->save()){
+            return response()->json($distributeur, 200); //HTTP Status code
         }
     }
 
@@ -169,10 +174,125 @@ class DistributeurController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
      * @return \Illuminate\Http\Response
+     *
+     *  @param  int  $id_distributeur
+     * @return \Illuminate\Http\Response
+     *
+     * @SWG\Put(
+     *     path="/distributeur/{id_distributeur}",
+     *     summary="Modify a discount.",
+     *     tags={"distributeur"},
+     *     operationId="putDistributeur",
+     *     produces={"application/xml", "application/json"},
+     *     @SWG\Parameter(
+     *         name="id_distributeur",
+     *         in="path",
+     *         description="Id of the distributeur",
+     *         required=true,
+     *         type="integer",
+     *     ),
+     *     @SWG\Parameter(
+     *         name="nom",
+     *         in="formData",
+     *         description="Nom du distributeur",
+     *         required=true,
+     *         type="string",
+     *     ),
+     *     @SWG\Parameter(
+     *         name="telephone",
+     *         in="formData",
+     *         description="Telephone",
+     *         required=true,
+     *         type="string",
+     *     ),
+     *     @SWG\Parameter(
+     *         name="adresse",
+     *         in="formData",
+     *         description="Adresse",
+     *         required=false,
+     *         type="string",
+     *     ),
+     *     @SWG\Parameter(
+     *         name="cpostal",
+     *         in="formData",
+     *         description="Code postal",
+     *         required=false,
+     *         type="string",
+     *     ),
+     *
+     *     @SWG\Parameter(
+     *         name="ville",
+     *         in="formData",
+     *         description="Ville",
+     *         required=false,
+     *         type="string",
+     *     ),
+     *     @SWG\Parameter(
+     *         name="pays",
+     *         in="formData",
+     *         description="Pays",
+     *         required=false,
+     *         type="string",
+     *     ),
+     *     @SWG\Response(
+     *          response=200,
+     *          description="successful operation",
+     *          @SWG\Schema(
+     *              type="array",
+     *              @SWG\Items(ref="#/definitions/Distributeur")
+     *          ),
+     *     ),
+     *     @SWG\Response(
+     *          response=404,
+     *          description="Woops! The discount that you looking for doesn't exist!",
+     *          @SWG\Schema(
+     *              type="array",
+     *              @SWG\Items(ref="#/definitions/Distributeur")
+     *          ),
+     *     ),
+     *     @SWG\Response(
+     *          response=422,
+     *          description="Woops! The informations can't be validated!",
+     *          @SWG\Schema(
+     *              type="array",
+     *              @SWG\Items(ref="#/definitions/Distributeur")
+     *          ),
+     *     ),
+     *     @SWG\Response(
+     *          response="default",
+     *          description="An error has been occured",
+     *     ),
+     * )
      */
     public function update(Request $request, $id)
     {
-        //
+        $distributeur = Distributeur::find($id);
+        if(empty($distributeur)){
+            return response()->json(
+                ['error' => 'Woops! The distributeur that you looking for doesn\'t exist!'],404); //HTTP Status code
+        }
+
+        $validator = Validator::make($request->all(),[
+            'nom'=>'required|string|unique:distributeurs,nom',
+            'telephone'=>'string',
+            'cpostal'=>'string',
+            'ville'=>'string',
+            'pays'=>'string'
+        ]);
+
+        if($validator->fails()){
+            return response()->json(
+                ['errors' => $validator->errors()->all()], 422); //HTTP Status code
+
+        }
+
+        $distributeur->fill(Input::all());
+        $distributeur->save();
+        return response()->json(
+            ['Fields have correctly been updated'],
+            200
+        );
+
     }
 
     /**
